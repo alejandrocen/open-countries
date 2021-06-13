df536b19b6e86619ab6e05fa860e6c261e57ac08
Alejandro Cen <alex96.cen@gmail.com>
2021-06-13 11:49:21 -0500

    Config mongoid

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,9 @@ require_relative 'boot'
 
 Bundler.require(:default, ENV['RACK_ENV'])
 
-require_relative '../api/core'
+Mongoid.load!('config/mongoid.yml')
+
+require_relative '../app/core'
 
 module API
   class Application < Grape::API

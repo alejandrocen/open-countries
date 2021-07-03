9567223bceb3286f14e31f51856172417c5a90ea
Alejandro Cen <alex96.cen@gmail.com>
2021-07-03 17:22:18 -0500

    Implement pagination

--- app/api/countries.rb
+++ app/api/countries.rb
@@ -3,8 +3,13 @@
 module API
   class Countries < Grape::API
     resource :countries do
+      params do
+        optional :limit, type: Integer, default: 20
+        optional :offset, type: Integer, default: 0
+      end
       get do
-        Country.all
+        countries = Country.limit(params[:limit]).offset(params[:offset])
+        { data: CountryEntity.represent(countries) }
       end
     end
   end
--- test/api/countries_test.rb
+++ test/api/countries_test.rb
@@ -6,7 +6,8 @@ class APICountriesTest < APITest
   def test_get_all_countries
     create_list(:country, 4)
     get '/api/countries'
+    json_response = JSON.parse(last_response.body)
 
-    assert_equal(JSON.parse(last_response.body).count, 4)
+    assert_equal(json_response['data']['countries'].count, 4)
   end
 end

304c61ea096c414828d73e8e335926c0e96a43bf
Alejandro Cen <alex96.cen@gmail.com>
2021-07-04 23:09:37 -0500

    Use Dir for load paths

--- config/setup.rb
+++ config/setup.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-DIRS = %w[app/models app/entities app/controllers app/repositories lib].freeze
-
 Bundler.require(:default, ENV['RACK_ENV'])
 Mongoid.load!('config/mongoid.yml')
 
 loader = Zeitwerk::Loader.new
-DIRS.each { |dir| loader.push_dir(dir) }
+Dir['app/*', 'lib'].each { |dir| loader.push_dir(dir) }
 loader.setup

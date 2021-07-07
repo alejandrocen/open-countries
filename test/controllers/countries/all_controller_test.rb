9a72df875a888031fcee73828eccd08deccc25a6
Alejandro Cen <alex96.cen@gmail.com>
2021-07-07 12:13:49 -0500

    Implement selected fields in all countries endpoint

--- test/controllers/countries/all_controller_test.rb
+++ test/controllers/countries/all_controller_test.rb
@@ -6,10 +6,19 @@ module Countries
   class AllControllerTest < ControllerTest
     def test_get_all_countries
       create_list(:country, 30)
-      get '/api/countries/all'
+      get '/api/countries/all', sort: '-alpha2_code'
       countries = JSON.parse(last_response.body).dig('data', 'countries')
 
       assert_equal(countries.count, Country.count)
+    end
+
+    def test_get_all_countries_with_sort
+      create_list(:country, 30)
+      get '/api/countries/all', sort: '-alpha2_code', fields: 'alpha2_code'
+      countries = JSON.parse(last_response.body).dig('data', 'countries')
+      alpha2_codes = countries.pluck('alpha2_code')
+
+      assert_equal(Country.order(alpha2_code: :desc).pluck(:alpha2_code), alpha2_codes)
     end
   end
 end
